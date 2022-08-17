7121587c2ee78630baec4d461d222c8e18b82c7b
sebaek <sebaek@gmail.com>
2022-08-17 12:39:27 +0900

    change

--- src/ch01/book/RunStatementExample.java
+++ src/ch01/book/RunStatementExample.java
@@ -6,5 +6,8 @@ public class RunStatementExample {
 		int y = 2;	// 변수 y를 선언하고 2를 저장
 		int result = x + y; 	// 변수 result를 선언하고 x와 y를 더한 값을 저장
 		System.out.println(x + "+" + y + "=" + result);	//콘솔에 출력하는 메소드 호출
+		
+		// 새로 추가된 코드
+		// 변경된 코드
 	}
 }
