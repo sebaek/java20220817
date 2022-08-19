6ad1e23843c13a14c6b4991868f28ec6d52d8bfa
sebaek <sebaek@gmail.com>
2022-08-19 14:31:51 +0900

    parrotTrouble

--- src/p99codingbat/WarmUp1.java
+++ src/p99codingbat/WarmUp1.java
@@ -1,6 +1,10 @@
 package p99codingbat;
 
 public class WarmUp1 {
+	public boolean parrotTrouble(boolean talking, int hour) {
+		return talking && (hour < 7 || hour > 20);
+	}
+
 	public int diff21(int n) {
 		int diff = 21 - n;
 
