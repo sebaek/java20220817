51a661f82926f4bd3970a4c9f5388b124ec1e0d6
sebaek <sebaek@gmail.com>
2022-08-22 14:22:06 +0900

    for

--- src/ch04/lecture/C09For.java
+++ src/ch04/lecture/C09For.java
@@ -17,5 +17,30 @@ public class C09For {
 			}
 			System.out.println();
 		}
+		
+		System.out.println("=====================");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 5; j >= 1; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("=====================");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 4; j >= 0; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
