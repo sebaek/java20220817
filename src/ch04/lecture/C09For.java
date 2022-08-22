65a9eca3b1a60f5888876d197ae1ba09e532cc9f
sebaek <sebaek@gmail.com>
2022-08-22 14:04:55 +0900

    for

---
+++ src/ch04/lecture/C09For.java
@@ -0,0 +1,16 @@
+package ch04.lecture;
+
+public class C09For {
+	public static void main(String[] args) {
+		System.out.println("======================");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("=====================");
+		
+	}
+}
