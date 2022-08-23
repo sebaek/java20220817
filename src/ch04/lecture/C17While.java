9ba6c57830290818e3e24c36c09800f2b86d6e19
sebaek <sebaek@gmail.com>
2022-08-23 10:19:13 +0900

    guess dice

---
+++ src/ch04/lecture/C17While.java
@@ -0,0 +1,30 @@
+package ch04.lecture;
+
+import java.util.Scanner;
+
+public class C17While {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("(주사위 맞추기)");
+		System.out.print("번호를 입력하세요(1~6):");
+		
+		int user = scanner.nextInt();
+		
+		int d = (int) (Math.random() * 6) + 1;
+		
+		System.out.println("주사위 결과 : " + d);
+
+		if (user == d) {
+			System.out.println("맞췄습니다.");
+		} else {
+			System.out.println("틀렸습니다.");
+		}
+	}
+}
+
+
+
+
+
