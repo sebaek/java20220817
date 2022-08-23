20099c96fee7f4c5b58a5791d22f00c82f0de81e
sebaek <sebaek@gmail.com>
2022-08-23 14:40:00 +0900

    parameter

---
+++ src/ch04/lecture/p02method/C06Parameter.java
@@ -0,0 +1,10 @@
+package ch04.lecture.p02method;
+
+public class C06Parameter {
+	public static void main(String[] args) {
+		
+		sum(1, 10); // 1~10사이의 정수 합 출력
+		sum(1, 100); // 1~100사이의 정수 합 출력
+		sum(50, 200); // 50~200
+	}
+}
