b9886ce5cc18cab6f8cc358371436006d69e0c22
sebaek <sebaek@gmail.com>
2022-08-23 16:25:41 +0900

    overloading

--- src/ch04/lecture/p02method/C12Overloading.java
+++ src/ch04/lecture/p02method/C12Overloading.java
@@ -5,8 +5,16 @@ public class C12Overloading {
 		double a = half(5.0);
 		System.out.println(a); // 2.5
 		
-		int b = half(5);
+		double b = half(5);
 		System.out.println(b); // 2
 		
+	}
+	
+	public static double half(int v) {
+		return v / 2;
+	}
+	
+	public static double half(double v) {
+		return v / 2;
 	}
 }
