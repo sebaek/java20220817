d3f51f5a6559452e42a649b88beeab692ee99465
sebaek <sebaek@gmail.com>
2022-08-23 14:45:41 +0900

    method

--- src/ch04/lecture/p02method/C06Parameter.java
+++ src/ch04/lecture/p02method/C06Parameter.java
@@ -7,4 +7,18 @@ public class C06Parameter {
 		sum(1, 100); // 1~100사이의 정수 합 출력
 		sum(50, 200); // 50~200
 	}
+	
+	public static void sum(int a, int b) {
+		long sum = 0;
+		
+		for (int i = a; i <= b; i++) {
+			sum += i;
+		}
+		
+		System.out.println(a + "와 " + b + " 사이의 정수의 합은 " + sum + "입니다.");
+	}
 }
+
+
+
+
