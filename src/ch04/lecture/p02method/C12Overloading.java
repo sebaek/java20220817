5fcac9d2f648f79806698523f08747015a4942cd
sebaek <sebaek@gmail.com>
2022-08-23 16:21:19 +0900

    overloading

---
+++ src/ch04/lecture/p02method/C12Overloading.java
@@ -0,0 +1,12 @@
+package ch04.lecture.p02method;
+
+public class C12Overloading {
+	public static void main(String[] args) {
+		double a = half(5.0);
+		System.out.println(a); // 2.5
+		
+		int b = half(5);
+		System.out.println(b); // 2
+		
+	}
+}
