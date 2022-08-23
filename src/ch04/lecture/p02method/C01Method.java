e0493c66285dfb163f6a0cc871d561d90d33f063
sebaek <sebaek@gmail.com>
2022-08-23 14:17:52 +0900

    method naming

--- src/ch04/lecture/p02method/C01Method.java
+++ src/ch04/lecture/p02method/C01Method.java
@@ -14,7 +14,11 @@ public class C01Method {
 		
 		method1();
 		System.out.println(5);
+		
+		method2();
 		System.out.println(6);
+		
+		method2();
 	}
 	
 	// 메소드(method)
@@ -22,6 +26,11 @@ public class C01Method {
 		System.out.println("a");
 		System.out.println("b");
 		System.out.println("c");
+	}
+	
+	// method2 메소드 작성하고 main에서 method2를 호출하는 코드 작성
+	public static void method2() {
+		System.out.println("가나다");
 	}
 }
 
