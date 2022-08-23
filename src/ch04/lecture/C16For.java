9433dfb08f0f63332af5e7b1c471ae4e93688b50
sebaek <sebaek@gmail.com>
2022-08-23 10:10:43 +0900

    scanner

--- src/ch04/lecture/C16For.java
+++ src/ch04/lecture/C16For.java
@@ -6,17 +6,18 @@ public class C16For {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-//		System.out.println("첫번째 정수 입력 :");
+		System.out.print("첫번째 정수 입력 :");
+		int first = scanner.nextInt();
 		
-		System.out.print("마지막 정수 입력 (1보다 큰수) :");
+		System.out.print("마지막 정수 입력 (" + first + "보다 큰수) :");
 		int last = scanner.nextInt();
 		
 		int sum = 0;
 		
-		for (int i = 1; i <= last; i++) {
+		for (int i = first; i <= last; i++) {
 			sum += i;
 		}
 		
-		System.out.println("1부터 " + last + "까지 합 : " + sum);
+		System.out.println(first + "부터 " + last + "까지 합 : " + sum);
 	}
 }
