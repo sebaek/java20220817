0b21e5a9ac27f2602f8b1e42a9eecc2b22b82678
sebaek <sebaek@gmail.com>
2022-08-24 11:20:11 +0900

    length

--- src/ch05/lecture/p01array/C06Array.java
+++ src/ch05/lecture/p01array/C06Array.java
@@ -15,12 +15,24 @@ public class C06Array {
 		printLastItem(arr2); // 50
 		printLastItem(arr3); // 101
 		printLastItem(new int[] {-1}); // -1
-		
+//		
 		printMiddleItem(arr1); // 8
 		printMiddleItem(arr2); // 50
 		printMiddleItem(arr3); // 100
 		printMiddleItem(new int[] {5, 6, 7, 8, 9}); // 7
 		printMiddleItem(new int[] {100, 200, 300, 400}); // 300
+	}
+	
+	private static void printMiddleItem(int[] crr) {
+		System.out.println(crr[crr.length / 2]);
+	}
+
+	private static void printLastItem(int[] brr) {
+		System.out.println(brr[brr.length - 1]);
+	}
+
+	public static void printFirstItem(int[] arr) {
+		System.out.println(arr[0]);
 	}
 }
 
