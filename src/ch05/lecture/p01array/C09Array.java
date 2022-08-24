d33a4b4e767fc771a894296c8dde5db206cadb67
sebaek <sebaek@gmail.com>
2022-08-24 16:05:32 +0900

    이차원 배열

---
+++ src/ch05/lecture/p01array/C09Array.java
@@ -0,0 +1,22 @@
+package ch05.lecture.p01array;
+
+public class C09Array {
+	public static void main(String[] args) {
+		int[][] a = {{3,4}, {7,8}, {99, 98}};
+		int[][] b = {{1,2,3}, {4,5,6}};
+		
+		System.out.println(a.length);
+		System.out.println(b.length);
+		
+		System.out.println(a[0].length); //2
+		System.out.println(a[1].length); //2
+		System.out.println(a[2].length); //2
+		
+		System.out.println(b[0].length); //3
+		System.out.println(b[1].length); //3
+		
+		
+		
+		
+	}
+}
