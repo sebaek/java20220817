3c76f79fff3eaa79c510582fa716ee79bdae2a13
sebaek <sebaek@gmail.com>
2022-08-24 12:02:04 +0900

    sameFirstLast

--- src/p99codingbat/Array1.java
+++ src/p99codingbat/Array1.java
@@ -1,6 +1,17 @@
 package p99codingbat;
 
 public class Array1 {
+	public boolean sameFirstLast(int[] nums) {
+		if (nums.length > 0) {
+			int first = nums[0];
+			int last = nums[nums.length - 1];
+
+			return first == last;
+		}
+
+		return false;
+	}
+
 	public boolean firstLast6(int[] nums) {
 		int first = nums[0];
 		int last = nums[nums.length - 1];
