d9d7dabf1ebc5617643568938a61294404cbc328
sebaek <sebaek@gmail.com>
2022-08-25 15:02:27 +0900

    centeredAverage

--- src/p99codingbat/Array2.java
+++ src/p99codingbat/Array2.java
@@ -1,6 +1,23 @@
 package p99codingbat;
 
 public class Array2 {
+	public int centeredAverage(int[] nums) {
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		int sum = 0;
+
+		// max, min, sum
+		for (int num : nums) {
+			max = Math.max(max, num);
+			min = Math.min(min, num);
+			sum += num;
+		}
+
+		sum = sum - max - min;
+
+		return sum / (nums.length - 2);
+	}
+
 	public int bigDiff(int[] nums) {
 		int max = Integer.MIN_VALUE;
 		int min = Integer.MAX_VALUE;
