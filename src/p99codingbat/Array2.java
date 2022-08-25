27a22753bc241dcc9da84b4a8dae8b3a6db668f4
sebaek <sebaek@gmail.com>
2022-08-25 14:38:42 +0900

    bigDiff

--- src/p99codingbat/Array2.java
+++ src/p99codingbat/Array2.java
@@ -1,6 +1,18 @@
 package p99codingbat;
 
 public class Array2 {
+	public int bigDiff(int[] nums) {
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+
+		for (int num : nums) {
+			max = Math.max(max, num);
+			min = Math.min(min, num);
+		}
+
+		return max - min;
+	}
+
 	public int countEvens(int[] nums) {
 		int cnt = 0;
 
