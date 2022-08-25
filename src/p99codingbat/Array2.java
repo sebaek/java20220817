de6ded8bd7f6b6e3f0e164fff98e9b832db2db8a
sebaek <sebaek@gmail.com>
2022-08-25 15:42:23 +0900

    sum67

--- src/p99codingbat/Array2.java
+++ src/p99codingbat/Array2.java
@@ -1,6 +1,28 @@
 package p99codingbat;
 
 public class Array2 {
+	public int sum67(int[] nums) {
+		int sum = 0;
+
+		boolean stop = false;
+
+		for (int num : nums) {
+			if (num == 6) {
+				stop = true;
+			}
+
+			if (!stop) {
+				sum += num;
+			}
+
+			if (num == 7) {
+				stop = false;
+			}
+		}
+
+		return sum;
+	}
+
 	public int sum13(int[] nums) {
 		int sum = 0;
 
