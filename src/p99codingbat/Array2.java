a185792e798b50158c0e0bc16a71d8358e446de2
sebaek <sebaek@gmail.com>
2022-08-25 15:15:16 +0900

    sum13

--- src/p99codingbat/Array2.java
+++ src/p99codingbat/Array2.java
@@ -1,6 +1,20 @@
 package p99codingbat;
 
 public class Array2 {
+	public int sum13(int[] nums) {
+		int sum = 0;
+
+		int before = 0;
+		for (int num : nums) {
+			if (num != 13 && before != 13) {
+				sum += num;
+			}
+			before = num;
+		}
+
+		return sum;
+	}
+
 	public int centeredAverage(int[] nums) {
 		int max = Integer.MIN_VALUE;
 		int min = Integer.MAX_VALUE;
