fb88a16fe3619a4b14b06c6a835677304ca265f2
sebaek <sebaek@gmail.com>
2022-09-01 15:12:22 +0900

    casting

---
+++ src/ch08/lecture/p01interface/C04Casting.java
@@ -0,0 +1,16 @@
+package ch08.lecture.p01interface;
+
+public class C04Casting {
+	public static void main(String[] args) {
+		CharSequence o1 = "topgun";
+		int len = o1.length();
+		char c = o1.charAt(3);
+		
+		String o2 = (String) o1;
+		
+		byte[] bytes = o2.getBytes();
+		
+		
+		
+	}
+}
