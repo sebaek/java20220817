12511ba19af3589e33764778dca577d82a4944f7
sebaek <sebaek@gmail.com>
2022-09-01 11:28:46 +0900

    interface 구현

---
+++ src/ch08/lecture/p01interface/MyClass3.java
@@ -0,0 +1,28 @@
+package ch08.lecture.p01interface;
+
+public class MyClass3 implements MyInterface3 {
+
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method4() {
+		// TODO Auto-generated method stub
+		
+	}
+}
