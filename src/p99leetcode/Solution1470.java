5b1bf6b840d5797755d5562baa646ca10d7f7547
sebaek <sebaek@gmail.com>
2022-09-02 15:39:58 +0900

    leetcode 1470

---
+++ src/p99leetcode/Solution1470.java
@@ -0,0 +1,15 @@
+package p99leetcode;
+
+public class Solution1470 {
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[nums.length];
+        
+        // your code..
+        for (int i = 0; i < n; i++) {
+            res[i * 2] = nums[i];
+            res[i * 2 + 1] = nums[i + n];
+        }
+        
+        return res;
+    }
+}
