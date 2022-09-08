82144beee12a27d55a067e1ffb96896b19d59d13
sebaek <sebaek@gmail.com>
2022-09-08 16:14:30 +0900

    1684 regex

--- src/p99leetcode/Solution1684.java
+++ src/p99leetcode/Solution1684.java
@@ -1,6 +1,21 @@
 package p99leetcode;
 
 public class Solution1684 {
+	//정규표현식 활용
+	public int countConsistentStrings(String allowed, String[] words) {
+        String pattern = "[" + allowed + "]+";
+        
+        int cnt = 0;
+        for (String word : words) {
+            if (word.matches(pattern)) {
+                cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+	
+	// if, for 사용
     public int countConsistentStrings(String allowed, String[] words) {
         int cnt = 0;
         for (String word : words) {
