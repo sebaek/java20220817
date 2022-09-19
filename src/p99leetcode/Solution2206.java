6641f46ad0d959ce6bb00730dfa4f5ec5a46fe31
sebaek <sebaek@gmail.com>
2022-09-19 11:16:06 +0900

    leetcode 2206

---
+++ src/p99leetcode/Solution2206.java
@@ -0,0 +1,28 @@
+package p99leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution2206 {
+    public boolean divideArray(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : nums) {
+            map.compute(num, (k, v) -> map.containsKey(k) ? v + 1 : 1);
+        }
+        
+        java.util.Collection<Integer> values = map.values();
+        
+        for (Integer value : values) {
+            if (value % 2 == 1) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+
+
+
