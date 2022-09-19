39f60dfd5c4555bd871c97c1a17a958c2d6d87c1
sebaek <sebaek@gmail.com>
2022-09-19 17:32:31 +0900

    boxed

--- src/ch16/lecture/p02stream/C08Mapping.java
+++ src/ch16/lecture/p02stream/C08Mapping.java
@@ -11,13 +11,18 @@ public class C08Mapping {
 				.forEach((e) -> System.out.println(e));
 		
 		// 
-		String res = IntStream.of(3, 5, 9, 1, 0)
 		// 코드 작성
-		
+		String res = IntStream.of(3, 5, 9, 1, 0)
+				.boxed()
+				.sorted((a, b) -> b - a)
+				.map((e) -> e + "")
+				.reduce((s1, s2) -> s1 + ", " + s2)
+				.get();
 		
 		System.out.println(res); // "9, 5, 3, 1, 0"
 	}
 }
+
 
 
 
