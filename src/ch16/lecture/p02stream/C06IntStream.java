22b8f7d41457460ce1e7ea5c0a62fb2d5e2ecf3a
sebaek <sebaek@gmail.com>
2022-09-19 15:30:55 +0900

    intstream 연습

---
+++ src/ch16/lecture/p02stream/C06IntStream.java
@@ -0,0 +1,20 @@
+package ch16.lecture.p02stream;
+
+public class C06IntStream {
+	public static void main(String[] args) {
+		// 1~10까지 출력
+		
+		// 1~100까지의 합 출력
+		
+		// 1~100까지의 수 중 홀 수의 합 출력
+		
+		// [5, 10, 20, 1, 6] 의 평균 구하기
+		
+		// 주사위(1~6) 5번 던진 결과 출력
+		
+		// 로또번호(1~45) 6개 작은 수 부터 출력 (중복없이)
+		
+		
+	}
+}
+
