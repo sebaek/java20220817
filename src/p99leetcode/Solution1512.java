1f6dc8739b70d86664337e2b1fa686e7dceb6586
sebaek <sebaek@gmail.com>
2022-09-20 16:08:53 +0900

    leetcode 1512

--- src/p99leetcode/Solution1512.java
+++ src/p99leetcode/Solution1512.java
@@ -1,9 +1,24 @@
 package p99leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Solution1512 {
+	// stream api
+	public int numIdenticalPairs(int[] nums) {
+		return Arrays.stream(nums)
+				.boxed()
+				.collect(Collectors.groupingBy(e -> e, Collectors.counting()))
+				.values()
+				.stream()
+				.mapToInt(n -> n.intValue())
+				.map(n -> n * (n - 1) / 2)
+				.sum();
+	}
+
+	// classic
 	public int numIdenticalPairs(int[] nums) {
 		Map<Integer, Integer> map = new HashMap<>();
 		for (int num : nums) {
