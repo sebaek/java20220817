90ff59b2ee66c628d9e4d10e6ac4bfc20439a996
sebaek <sebaek@gmail.com>
2022-09-20 16:16:26 +0900

    leetcode 2206 w/ stream api

--- src/p99leetcode/Solution2206.java
+++ src/p99leetcode/Solution2206.java
@@ -4,25 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution2206 {
-    public boolean divideArray(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int num : nums) {
-            map.compute(num, (k, v) -> map.containsKey(k) ? v + 1 : 1);
-        }
-        
-        java.util.Collection<Integer> values = map.values();
-        
-        for (Integer value : values) {
-            if (value % 2 == 1) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
+	// stream api
+	public boolean divideArray(int[] nums) {
+		return Arrays.stream(nums)
+				.boxed()
+				.collect(Collectors.groupingBy((e) -> e, Collectors.counting()))
+				.values()
+				.stream()
+				.allMatch(e -> e % 2 == 0);
+	}
+
+	// classic
+	public boolean divideArray(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int num : nums) {
+			map.compute(num, (k, v) -> map.containsKey(k) ? v + 1 : 1);
+		}
+
+		java.util.Collection<Integer> values = map.values();
+
+		for (Integer value : values) {
+			if (value % 2 == 1) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
-
-
-
-
