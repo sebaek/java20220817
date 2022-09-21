1a82d5309572455a58291ce6027a8103077784f7
sebaek <sebaek@gmail.com>
2022-09-21 12:11:41 +0900

    try with resources

--- src/ch18/lecture/p3exception/C04Exception.java
+++ src/ch18/lecture/p3exception/C04Exception.java
@@ -7,6 +7,8 @@ public class C04Exception {
 	public static void main(String[] args) {
 		// 생성시 checked exception 처리하지 않아도 되고
 		// 다시 할당하는 코드가 작성되지 않는다면(effectively final)
+		// try 블럭 전에 선언 및 할당하고
+		// try () 변수명 나열할 수 있음
 		// java 9부터 가능
 
 		final Scanner scanner = new Scanner("");
