67ef23cfeb45cf0760479a8a53a9beb8c5bb9efd
sebaek <sebaek@gmail.com>
2022-09-22 14:30:24 +0900

    add thread to server

--- src/ch18/lecture/p7network/C05Server.java
+++ src/ch18/lecture/p7network/C05Server.java
@@ -12,27 +12,46 @@ public class C05Server {
 		System.out.println("연결 기다리는 중...");
 		try (
 				ServerSocket serverSocket = new ServerSocket(55000);
-				Socket socket = serverSocket.accept();
-				InputStream is = socket.getInputStream();
-				BufferedInputStream bis = new BufferedInputStream(is);) {
-			String remoteAddress = socket.getRemoteSocketAddress().toString();
-			String clientIpAddress = remoteAddress.substring(0, remoteAddress.indexOf(':'));
-			String folder = "C:\\Users\\user\\Desktop\\output\\";
-			String fileName = folder + clientIpAddress.replace('.', '_') + ".dat";
 
-			try (
-					FileOutputStream fos = new FileOutputStream(fileName);
-					BufferedOutputStream bos = new BufferedOutputStream(fos);) {
-				byte[] buf = new byte[1000];
-				int len = 0;
-				int fileSize = 0;
-				while ((len = bis.read(buf)) != -1) {
-					fileSize += len;
-					bos.write(buf, 0, len);
-				}
-				bos.flush();
-				System.out.println(clientIpAddress + "로 부터");
-				System.out.println(fileSize + "bytes 크기의 파일을 받았습니다.");
+		) {
+
+			int cnt = 0;
+			while (cnt < 100) {
+				cnt++;
+				Socket socket = serverSocket.accept();
+				new Thread(() -> {
+
+					String remoteAddress = socket.getRemoteSocketAddress().toString();
+					String clientIpAddress = remoteAddress.substring(0, remoteAddress.indexOf(':'));
+					String folder = "C:\\Users\\user\\Desktop\\output\\";
+					String fileName = folder + clientIpAddress.replace('.', '_') + ".dat";
+
+					try (
+							InputStream is = socket.getInputStream();
+							BufferedInputStream bis = new BufferedInputStream(is);
+							FileOutputStream fos = new FileOutputStream(fileName);
+							BufferedOutputStream bos = new BufferedOutputStream(fos);) {
+						byte[] buf = new byte[1000];
+						int len = 0;
+						int fileSize = 0;
+						while ((len = bis.read(buf)) != -1) {
+							fileSize += len;
+							bos.write(buf, 0, len);
+						}
+						bos.flush();
+						System.out.println(clientIpAddress + "로 부터");
+						System.out.println(fileSize + "bytes 크기의 파일을 받았습니다.");
+
+						if (socket != null) {
+							socket.close();
+						}
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				}).start();
+
 			}
 
 		} catch (Exception e) {
