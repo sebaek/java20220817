f97f3031b3b20b34eb87b012508aa30746b74ade
sebaek <sebaek@gmail.com>
2022-09-22 11:22:09 +0900

    server w/ thread

--- src/ch18/lecture/p7network/C01Server.java
+++ src/ch18/lecture/p7network/C01Server.java
@@ -2,6 +2,7 @@ package ch18.lecture.p7network;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class C01Server {
 	public static void main(String[] args) {
@@ -11,32 +12,29 @@ public class C01Server {
 		// ServerSocket 사용
 
 		System.out.println("연결 기다림");
+		// 172.30.1.60:60000
 		try (
 				// ServerSocket 만듦
-				ServerSocket serverSocket = new ServerSocket(60000);
-				
+				ServerSocket serverSocket = new ServerSocket(60000);) {
+			
+			AtomicInteger cnt = new AtomicInteger();
+			while (cnt.get() < 50) {
 				// 연결 기다림
-				Socket socket = serverSocket.accept();) {
+				Socket socket = serverSocket.accept();
+				cnt.incrementAndGet();
+				
+				Thread thread = new Thread(() -> {
+					System.out.println(socket.getRemoteSocketAddress());
+					
+					System.out.println("연결 종료");
+				});
+				thread.start();
+				
+			}
 
-			System.out.println(socket.getRemoteSocketAddress());
-			
-			System.out.println("연결 종료");
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
