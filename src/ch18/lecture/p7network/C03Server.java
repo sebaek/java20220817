a7e987b62c54bd7c175478430896c6f445972721
sebaek <sebaek@gmail.com>
2022-09-22 12:15:27 +0900

    server

--- src/ch18/lecture/p7network/C03Server.java
+++ src/ch18/lecture/p7network/C03Server.java
@@ -9,31 +9,36 @@ import java.net.Socket;
 public class C03Server {
 	public static void main(String[] args) {
 		System.out.println("연결 시도 중...");
-		try (
-				ServerSocket serverSocket = new ServerSocket(60000);
-				Socket socket = serverSocket.accept();) {
+		try (ServerSocket serverSocket = new ServerSocket(60000);) {
+			int cnt = 0;
+			while (cnt < 50) {
+				cnt++;
+				Socket socket = serverSocket.accept();
+				System.out.println("연결 완료");
 
-			System.out.println("연결 완료");
-			
-			InputStream is = socket.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-			
-			try (is;isr;br;) {
-				
-				String line = "";
-				String from = socket.getRemoteSocketAddress().toString();
-				while ((line = br.readLine()) != null) {
-					System.out.println(from + "에서 받은메세지:" + line);
-				}
+				Thread thread = new Thread(() -> {
+					try (InputStream is = socket.getInputStream();
+							InputStreamReader isr = new InputStreamReader(is);
+							BufferedReader br = new BufferedReader(isr);) {
+
+						String line = "";
+						String from = socket.getRemoteSocketAddress().toString();
+						while ((line = br.readLine()) != null) {
+							System.out.println(from + "에서 받은메세지:" + line);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				});
+				thread.start();
+
 			}
-			
+
 			System.out.println("프로그램 종료");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
 }
